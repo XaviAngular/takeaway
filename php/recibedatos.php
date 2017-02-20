aeb59c1699af0e2c083c789b303ff0a1102aed14
XaviAngular <xavcet@gmail.com>
2017-02-20 18:46:53 +0100

    Mostrando variables recibidas por el post

--- php/recibedatos.php
+++ php/recibedatos.php
@@ -1,7 +1,10 @@
 <?php
 echo "<h1>Hola, aquí llegará el form</h1>";
 if ($_POST) {
-	echo "LLegan variables por post";
+	echo "LLegan variables por post:<br>";
+	echo $_POST['nombreApellido'];
+	echo "<br>";
+	echo $_POST['email'];
 	}
 else {
 	echo "No llega nada por post";

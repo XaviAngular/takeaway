23ad1d822276e52290395ce3642b2705b68f0b86
XaviAngular <xavcet@gmail.com>
2017-02-21 16:39:31 +0100

    Con test de insert into funcionando

--- php/recibedatos.php
+++ php/recibedatos.php
@@ -1,5 +1,6 @@
 <?php
 echo "<h1>Hola, aquí llegará el form</h1>";
+//Se comprueba que llegan datos por método POST
 if ($_POST) {
 	echo "LLegan variables por post:<br>";
 	echo $_POST['nombreApellido'];
@@ -9,11 +10,18 @@ if ($_POST) {
 	echo $_POST['mensaje'];
 	$mysqli = new mysqli('127.0.0.1', 'root', '', 'takeaway');
 	if ($mysqli){
-		$sql="";
+		$sql="INSERT INTO contactform ( nombreApellidos, email, mensaje) VALUES ( 'Josep Pla', 'mailto@taa.com', 'Mensaje de test');";
+		$query=$mysqli->query($sql);
+		if ($query) {
+			echo "Se ha grabado correctamente la información";
+		}
+		else 
+		{
+			echo "Ha habido un problema con el registro del formulario";
+		}
+		$mysqli->close();
 	}
-	$mysqli->close();
 }
-	
 else {
 	echo "No llega nada por post";
 }

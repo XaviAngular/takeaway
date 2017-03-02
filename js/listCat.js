2e9d3d694460414bbaf73076162e3e85cd44f09b
XaviAngular <xavcet@gmail.com>
2017-03-02 16:45:02 +0100

    Archivos para listado categorías

---
+++ js/listCat.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    var debug=false;
+    $.ajax({
+      url: '../php/getListCat.php',
+      type: 'GET',
+      dataType: 'json',
+      success : function(result){
+        console.log(result);
+        var tbl_body = "";
+        //Recorrer el array de la query que manda el php
+        $.each(result.query, function() {
+            if (debug) console.log("Pintando");
+            var tbl_row = "";
+            $.each(this, function(k , v) {
+              tbl_row += "<td>"+v+"</td>";
+            });            
+            tbl_body += "<tr>"+tbl_row+"</tr>";
+        });
+        if (debug) console.log(tbl_body);
+        $("#listado tbody").html(tbl_body);       
+      },
+      error: function(result){
+        alert("errorrrrrr!!!");
+      }
+    });    
+});     
---
+++ php/getListCat.php
@@ -0,0 +1,34 @@
+<?php
+//Para comprobar si se recibe un post desde un ajax
+if ($_SERVER['REQUEST_METHOD']==='GET'){	
+	$sql = "SELECT * FROM categoria";
+	$mysqli = new mysqli('127.0.0.1', 'root', '', 'takeaway');
+	mysqli_set_charset($mysqli,"utf8");
+	if ($mysqli) {
+		$query=$mysqli->query($sql);
+		$mysqli->close();
+		$rows = $query->fetch_all(MYSQLI_ASSOC);
+		}
+	if ($query) {
+	echo json_encode([
+		"query" 	=> $rows,
+		"error"		=> 0,	
+		"resultado" => "se ha cargado"
+		]);	
+	}
+	else {
+		echo json_encode([
+		"query" 	=> $query,
+		"error"		=> 1,
+		"resultado" => "NO se ha cargado!!"
+		]);	
+	}
+}
+else {
+echo json_encode([
+		"query" => "KO",
+		"error"		=> 1,
+		"resultado"	=> "no hay"
+	]);
+}
+?>

c087acd64ad80144ed58004252799c7907d21fb1
XaviAngular <xavcet@gmail.com>
2017-03-06 16:42:50 +0100

    Archivos para crear nuevo formulario, parte que se envían los datos para el registro

--- js/addPlato.js
+++ js/addPlato.js
@@ -30,4 +30,38 @@ $(document).ready(function() {
         alert("errorrrrrr!!!");
       }
     });
-     });   
+
+  //Ajax para enviar formulario
+  $("#formPlato").submit(function(event){
+      event.preventDefault();//Evita el refresh automático que se produce al enviar el form
+      //esta es la forma de obtener datos del formulario y 
+      //convertirlo en formato JSON
+      var jsonData= JSON.stringify($("#formPlato").serializeArray());
+      if (debug) console.log("Datos en Json:");
+      if (debug) console.log(jsonData);
+      $.ajax({
+        url: '../php/recibePlato.php',
+        type: 'POST',
+        dataType: 'json',
+        data: jsonData,
+        success : function(result){
+          console.log(result.sql);
+          if (result.error===0) {
+            Materialize.toast('Plato creado!', 4000); // 4000 is the duration of the toast
+            //Borrar el contenido del formulario
+            $('#formPlato')[0].reset();
+            //tambien se puede usar para resetear el form
+            $('#formPlato').trigger("reset");
+          }
+          else {
+            Materialize.toast('Error al crear categoría!', 6000); // 4000 is the duration of the toast
+            $('#formPlato')[0].reset();
+          }
+          
+        },
+        error: function(result){
+          alert("errorrrrrr!!!");
+        }
+      });
+    });    
+});   

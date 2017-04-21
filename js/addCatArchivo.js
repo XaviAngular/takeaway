479db9fc546aa17c4ffc0bf5545983c8f9e6d778
XaviAngular <xavcet@gmail.com>
2017-04-21 18:25:30 +0200

    En clase: Archivos para crear categorías con subida de archivos.

---
+++ js/addCatArchivo.js
@@ -0,0 +1,61 @@
+var files;
+$(document).ready(function() {
+    var debug=true;
+    $('select').material_select();
+
+    //Se prepara evento para capturar el archivo
+    $('input[type=file]').on('change',function(event){
+      files= event.target.files;
+      console.log(files);
+    })
+    
+
+
+
+    $("#formCat").submit(function(event){
+      event.preventDefault();//Evita el refresh automático que se produce al enviar el form
+      
+      var formElement = document.getElementById("formCat");
+      var miForm = new FormData(formElement);
+      
+      //añadir el archivo al formdata para enviar
+      $.each(files,function(key,value){
+        miForm.append(key,value);
+      })
+      
+      //Forma de mostrar por consola un objeto FormData
+      for (var pair of miForm.entries()){
+          console.log(pair[0]+', '+pair[1]);
+      }
+
+      
+      $.ajax({
+        url: '../php/recibeCatArchivo.php',
+        type: 'POST',
+        dataType: 'json',
+        data: miForm,
+        processData: false,
+        contentType:false,
+        success : function(result){
+          console.log(result.sql);
+          if (result.error===0) {
+            Materialize.toast('Categoría creada!', 4000); // 4000 is the duration of the toast
+            //Borrar el contenido del formulario
+            $('#formCat')[0].reset();
+            //tambien se puede usar para borrar el form
+            $('#formCat').trigger("reset");
+          }
+          else {
+            Materialize.toast('Error al crear categoría!', 6000); // 4000 is the duration of the toast
+            $('#formCat')[0].reset();
+          }
+          
+        },
+        error: function(result){
+          alert("errorrrrrr!!!");
+        }
+      })
+      
+
+    });    
+});     
---
+++ php/recibeCatArchivo.php
@@ -0,0 +1,59 @@
+<?php
+//Para comprobar si se recibe un post desde un ajax
+if ($_POST){
+	
+	if ($_FILES) {
+		$files = array();
+		$uploadDir= "../img/cats/";
+
+foreach($_FILES as $file){
+	if (move_uploaded_file($file['tmp_name'], "$uploadDir".basename($file['name'])))
+	{
+		$files[]= $uploadDir.$file['name'];
+	}
+	else {
+		$files[]="no se ha subido archivo";
+		}
+	}
+	}
+
+	$nombre = $_POST['nombre'];
+	$descripcion= $_POST['descripcion'];
+	$foto = "img/cats/".$_POST['foto'];
+
+	$sql = "INSERT INTO categoria (nombre,descripcion,foto) VALUES ('$nombre','$descripcion','$foto')";
+	
+	$mysqli = new mysqli('127.0.0.1', 'root', '', 'takeaway');
+	mysqli_set_charset($mysqli,"utf8");
+	if ($mysqli) {
+		$query=$mysqli->query($sql);
+		$mysqli->close();
+	}
+
+	if ($query) {
+	echo json_encode([
+		"campos" 	=> "no hay",
+		"error"		=> 0,
+		"valores"	=> "no hay",
+		"sql"		=> $sql,
+		"resultado" => "se ha grabado"
+		]);	
+	}
+	else {
+		echo json_encode([
+		"campos" 	=> "no hay",
+		"error"		=> 1,
+		"valores"	=> "no hay",
+		"sql"		=> $sql,
+		"resultado" => "NO se ha grabado!!"
+		]);	
+	}
+}
+else {
+echo json_encode([
+		"campos" => "KO",
+		"error"		=> 1,
+		"valores"	=> "no hay"
+	]);
+}
+?>
